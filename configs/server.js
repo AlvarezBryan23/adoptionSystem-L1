380b59fe1cef47617258338eccd825a1f97b3ee8
Informatica <informatica@alumno.local>
2025-02-04 14:47:57 -0600

    Seguido del proyecto de adopción

--- configs/server.js
+++ configs/server.js
@@ -6,8 +6,8 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import { dbConnection } from './mongo.js'
 import authRoutes from "../src/auth/auth.routes.js"
+import userRoutes from "../src/user/user.routes.js"
 import apiLimiter from '../src/middlewares/validar-cant-peticiones.js'
-
 
 const middlewares = (app) =>{
     app.use(express.urlencoded({extended: false}))
@@ -20,6 +20,7 @@ const middlewares = (app) =>{
 
 const routes = (app) => {
     app.use("/adoptionSystem/v1/auth", authRoutes)
+    app.use("/adoptionSystem/v1/user", userRoutes)
 }
 
 const connectarDB = async () => {
--- src/middlewares/check-validator.js
+++ src/middlewares/check-validator.js
@@ -1,6 +1,9 @@
-import { body } from "express-validator";
-import { existeEmail } from "../helpers/db-validators.js";
+import { body, param } from "express-validator";
+import { existeEmail, existeUsername, userExists } from "../helpers/db-validators.js";
 import { validarCampos } from "./validar-campos.js";
+import { deleteFileOnError } from "./delete-file-on-error.js";
+import { getUserById } from "../user/user.controller.js";
+
 
 export const registerValidator = [
     body("name", "El nombre es obligatorio").not().isEmpty(),
@@ -8,6 +11,7 @@ export const registerValidator = [
     body("email", "El correo es obligatorio").not().isEmpty(),
     body("email", "Ingrese un correo valido").isEmail(),
     body("email").custom(existeEmail),
+    body("username").custom(existeUsername),
   /*  body("password").isStrongPassword({
         minLength: 8,
         minLowercase: 1,
@@ -15,5 +19,36 @@ export const registerValidator = [
         minNumbers: 1,
         minSymbols: 0
     }),*/
-    validarCampos
+    validarCampos,
+    deleteFileOnError
+]
+
+export const loginValidator = [
+    body("email").optional().isEmail().withMessage("Ingrese un correo válido"),
+    body("username").optional().isString().withMessage("Ingrese un username válido"),
+    body("password").isLength({min: 8}).withMessage("La Contraseña debe tener 8 caracteres"),
+    validarCampos,
+    deleteFileOnError
+]
+
+export const getUserById = [
+    body("uid").isMongoId().withMessage("No es un ID válido"),
+    param("uid").custom(userExists),
+    validarCampos,
+    deleteFileOnError
+]
+
+export const deleteUserValidator = [
+    body("uid").isMongoId().withMessage("No es un ID válido"),
+    param("uid").custom(userExists),
+    validarCampos,
+    deleteFileOnError
+]
+
+export const updatePasswordValidator = [
+    body("uid").isMongoId().withMessage("No es un ID válido"),
+    param("uid").custom(userExists),
+    body("newPassword").isLength({min: 8}).withMessage("La contraseña debe tener al menos 8 caracteres"),
+    validarCampos,
+    deleteFileOnError
 ]
